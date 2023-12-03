156e5914bdb74a004580125191ae0b17f6c0d204
Carlos L <87445964+inova-tecnologia@users.noreply.github.com>
2023-12-03 14:28:30 -0300

    clean code adjustments

--- src/contactManager/ContactManager.java
+++ src/contactManager/ContactManager.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ContactManager implements Serializable {
-
 	private Map<String, String> contacts;
 
 	public ContactManager() {
@@ -37,8 +36,7 @@ public class ContactManager implements Serializable {
 	}
 
 	public void saveContactsToFile(String fileName) {
-	    try (FileOutputStream fileOut = new FileOutputStream(fileName);
-	         ObjectOutputStream objOut = new ObjectOutputStream(fileOut)) {
+	    try (FileOutputStream fileOut = new FileOutputStream(fileName); ObjectOutputStream objOut = new ObjectOutputStream(fileOut)) {
 	        objOut.writeObject(contacts);
 	        System.out.println("Contacts saved successfully!");
 	    } catch (IOException e) {
@@ -47,14 +45,11 @@ public class ContactManager implements Serializable {
 	}
 
 	public void readContactsFromFile(String fileName) {
-	    try (FileInputStream fileIn = new FileInputStream(fileName);
-	         ObjectInputStream objIn = new ObjectInputStream(fileIn)) {
+	    try (FileInputStream fileIn = new FileInputStream(fileName); ObjectInputStream objIn = new ObjectInputStream(fileIn)) {
 	        contacts = (Map<String, String>) objIn.readObject();
 	        System.out.println("Contacts read successfully!");
 	    } catch (IOException | ClassNotFoundException e) {
 	        System.out.println("Error while reading contacts: " + e.getMessage());
 	    }
 	}
-	
 }
-
